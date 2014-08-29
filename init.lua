91bf2ad1c8f07e1942410f81e7fe1e86b4ab8a5a
Calinou <calinou@opmbx.org>
2014-08-29 19:44:56 +0200

    Run nodeupdate when using Admin Pickaxe.

--- init.lua
+++ init.lua
@@ -527,6 +527,7 @@ minetest.register_on_punchnode(function(pos, node, puncher)
 	and minetest.get_node(pos).name ~= "air" then
 		minetest.log("action", puncher:get_player_name() .. " digs " .. minetest.get_node(pos).name .. " at " .. minetest.pos_to_string(pos) .. " using an Admin Pickaxe.")
 		minetest.remove_node(pos) -- The node is removed directly, which means it even works on non-empty containers and group-less nodes.
+		nodeupdate(pos) -- Run node update actions like falling nodes.
 	end
 end)
 
